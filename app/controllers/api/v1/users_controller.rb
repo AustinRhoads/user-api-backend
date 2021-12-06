bffb85e313ca75825393e852bcf1c193589dfb7b
Austin Rhoads <austinrrhoads@ymail.com>
2021-12-06 13:04:50 -0600

    gitignore update

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,14 +1,42 @@
 class api::v1::UsersController < ApplicationController
 
     def index
+        @users = User.all
+
+        if @users 
+
+            render json: {
+                status: 200,
+                users: @users
+            }
+
+        else 
+            render json: {
+                status: 500, 
+                errors: ['no users found']
+            }
+
+        end
 
     end
 
     def create 
 
+        @user = User.new(user_params)
+
+        if @user.save 
+
+
+
+        else
+
+        end
+
     end
 
     def show
+
+       @user = User.find(params[:id]) 
 
     end
 
---
+++ app/helpers/auth_helper.rb
@@ -0,0 +1,10 @@
+module AuthHelper
+
+    def self.encode_uid(user_id)
+
+    end
+
+    def self.decode_uid(user_id)
+
+    end
+end
